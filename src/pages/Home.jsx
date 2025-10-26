e755819f8507f0ec297d2f7b26b112ec802329c2
Spoorthi G <spoorthiggowda917@gmai.com>
2025-10-26 14:47:01 +0530

    added herosection and feauters

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from '../Components/Home/Banner'
+import HeroSection from '../Components/HeroSection'
+import Feature from '../Components/Home/Feature'
 
 function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+     <Banner/>
+     <HeroSection/>
+     <Feature/>
     </div>
   )
 }
