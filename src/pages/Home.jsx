d729fdb798fa60fb2f1bd130c80579ac344cc7d2
Spoorthi G <spoorthiggowda917@gmai.com>
2025-10-28 14:26:23 +0530

    added CallToAction section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Banner from '../Components/Home/Banner'
-import HeroSection from '../Components/HeroSection'
+import HeroSection from '../Components/Home/HeroSection'
 import Feature from '../Components/Home/Feature'
 import Testimonial from '../Components/Home/Testimonial'
+import CallToAction from '../Components/Home/CallToAction'
+
 
 
 function Home() {
@@ -12,6 +14,7 @@ function Home() {
      <HeroSection/>
      <Feature/>
      <Testimonial/>
+     <CallToAction/>
     </div>
   )
 }
