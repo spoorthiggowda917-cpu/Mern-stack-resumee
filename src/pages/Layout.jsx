e6bd0f7f0fd0cfb1c2290caadb7213596b470b4a
Spoorthi G <spoorthiggowda917@gmai.com>
2025-10-28 20:56:48 +0530

    added Navbar section

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../Components/Navbar'
 
 function Layout() {
   return (
     <div>
-      <h1>Layout page</h1>
-      <div>
+      <div className='min-h-screen bg-gray-50'>
+        <Navbar/>
         <Outlet/>{/*“Render the child route component here.”*/}
       </div>
     </div>
